5fadb2278da3cbc0ed7055214299fcc9b912543e
AndreLucyo2 <andre.lucyo2@gmail.com>
2023-07-24 22:40:43 -0300

    usanso o querySelectorAll obtem o array de botões

--- js/main.js
+++ js/main.js
@@ -4,6 +4,8 @@ const somar = document.querySelector("#somar");
 const subtrair = document.querySelector("#subtrair");
 const braco = document.querySelector("#braco");
 
+const controle = document.querySelectorAll('.controle-ajuste');
+console.log(controle);
 
 somar.addEventListener("click", () => {
     manipulaDados('somar');
