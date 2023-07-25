7fa523979092662139b6d184947994643e53b9cc
AndreLucyo2 <andre.lucyo2@gmail.com>
2023-07-24 23:10:44 -0300

    tornando o codigo mais dinamico

--- js/main.js
+++ js/main.js
@@ -4,12 +4,12 @@ const somar = document.querySelector("#somar");
 const subtrair = document.querySelector("#subtrair");
 const braco = document.querySelector("#braco");
 
-const controle = document.querySelectorAll('.controle-ajuste');
+const controle = document.querySelectorAll('[data-controle]');
 
 controle.forEach((elelento) => {
     elelento.addEventListener('click', (evento) => {
 
-        manipulaDados(evento.target.textContent, evento.target.parentNode);
+        manipulaDados(evento.target.dataset.controle, evento.target.parentNode);
 
         console.log(evento.target.textContent);
         console.log(evento.target.parentNode);
@@ -21,7 +21,7 @@ controle.forEach((elelento) => {
 function manipulaDados(operacao, controle) {
 
     //a partir do pai, obtem apenas o que quer alterar:
-    const peca = controle.querySelector(".controle-contador")
+    const peca = controle.querySelector("[data-contador]")
 
     if (operacao === '+') {
         peca.value = parseInt(peca.value) + 1;
