c0e979a98f74c679866319b3f3bfbdb9a23aca29
AndreLucyo2 <andre.lucyo2@gmail.com>
2023-07-26 22:02:58 -0300

    ajustado calculo do robo

--- js/main.js
+++ js/main.js
@@ -1,18 +1,46 @@
-const roboTron = document.querySelector("#roboTron");
-
-const somar = document.querySelector("#somar");
-const subtrair = document.querySelector("#subtrair");
-const braco = document.querySelector("#braco");
-
 const controle = document.querySelectorAll('[data-controle]');
+const estatistica = document.querySelectorAll('[data-estatistica]');
+
+const pecas = {
+    "bracos": {
+        "forca": 29,
+        "poder": 35,
+        "energia": -21,
+        "velocidade": -5
+    },
+
+    "blindagem": {
+        "forca": 41,
+        "poder": 20,
+        "energia": 0,
+        "velocidade": -20
+    },
+    "nucleos": {
+        "forca": 0,
+        "poder": 7,
+        "energia": 48,
+        "velocidade": -24
+    },
+    "pernas": {
+        "forca": 27,
+        "poder": 21,
+        "energia": -32,
+        "velocidade": 42
+    },
+    "foguetes": {
+        "forca": 0,
+        "poder": 28,
+        "energia": 0,
+        "velocidade": -2
+    }
+}
+
 
 controle.forEach((elelento) => {
     elelento.addEventListener('click', (evento) => {
 
         manipulaDados(evento.target.dataset.controle, evento.target.parentNode);
-
-        console.log(evento.target.textContent);
-        console.log(evento.target.parentNode);
+        atualizaEstatistica(evento.target.dataset.peca)
 
     })
 })
@@ -30,4 +58,13 @@ function manipulaDados(operacao, controle) {
         peca.value = parseInt(peca.value) - 1;
 
     }
+}
+
+function atualizaEstatistica(peca) {
+    estatistica.forEach((elelento) => {
+        elelento.textContent =
+            (parseInt(elelento.textContent) +
+                pecas[peca][elelento.dataset.estatistica])
+    })
+
 }
